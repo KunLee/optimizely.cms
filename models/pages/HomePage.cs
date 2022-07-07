f80c0734da9ce4bedc1128b313d8f204f91f3e6c
meimi <meimi@DESKTOP-S16GCMO>
2022-07-07 17:08:30 +0800

    Add project files.

---
+++ models/pages/HomePage.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace repos.models.pages
+{
+    [ContentType(GUID = "32DD2FAA-905F-4F6E-99B6-59DEF7FD2F80", DisplayName ="HomePage")]
+    public class HomePage: PageData
+    {
+        [Display(Order =100, GroupName =SystemTabNames.Content)]
+        public virtual ContentArea? Main { set; get; }
+    }
+}

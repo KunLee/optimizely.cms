ca90840918ad18e8236afd06bf394203cd90f05b
Lee Richard <kun.leeing@gmail.com>
2022-07-12 18:51:34 +0800

    new lookup block addition

---
+++ optimizely/models/blocks/ElementBlock/LookupTextboxElementBlock.cs
@@ -0,0 +1,10 @@
+﻿using EPiServer.Forms.Implementation.Elements;
+using EPiServer.Forms.Implementation.Elements.BaseClasses;
+
+namespace repos.models.blocks.ElementBlock
+{
+    [ContentType(GUID = "29D86CE3-3874-4E2E-B6DA-BE042FA996FF", GroupName = "ExtendedElements", Order = 2000)]
+    public class LookupTextboxElementBlock : InputElementBlockBase
+    {
+    }
+}
